5c5b77b784643be9a7faa4566426c305d3a5131c
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-09-19 23:42:54 +0300

    Create 02.LastKNumbersSequence.js

---
+++ JS Advanced - Exercises/02.LastKNumbersSequence.js
@@ -0,0 +1,14 @@
+function solve(n, k) {
+    let sequence = [1];
+
+    for (let i = 1; i < n; i++) {
+        let sum = 0;
+        let start = Math.max(0, i - k);
+        for (let j = start; j < i; j++) {
+            sum += sequence[j];
+        }
+    
+        sequence.push(sum);
+    }
+    return sequence
+}
