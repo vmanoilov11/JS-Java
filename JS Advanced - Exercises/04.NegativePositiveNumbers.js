b6153145698bc01ee2e431f7b0ca89f5fa278a11
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-09-19 23:45:04 +0300

    Create 04.NegativePositiveNumbers.js

---
+++ JS Advanced - Exercises/04.NegativePositiveNumbers.js
@@ -0,0 +1,13 @@
+function solve(array) {
+    let result = []
+    for(let num of array){
+        if(num < 0){
+            result.unshift(num)
+        }else{
+            result.push(num)
+        } 
+    }
+    for(let num of result){
+        console.log(num)
+    }
+}
