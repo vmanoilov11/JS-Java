13b4624f2425d8e82f4b7ae596658956e4b603db
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-09-29 10:28:04 +0300

    Create greet.java

---
+++ Java-Fundamental/greet.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, "+ name + "!");
+        
+    }
+}
