e05d435753a63b2c3810eeb152efb3871d002fa3
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-09-29 10:27:03 +0300

    Create rectArea.java

---
+++ Java-Fundamental/rectArea.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Area {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int area = a * b;
+        System.out.println(area);
+    }
+}
