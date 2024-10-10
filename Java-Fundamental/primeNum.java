1ce4d1e48e616cacfa318b8c4cea7b34792e2280
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-10-10 08:38:38 +0300

    Create primeNum.java

---
+++ Java-Fundamental/primeNum.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
