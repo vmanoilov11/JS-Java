67703d289da022fa3f43a4ce7ec3c62e92fc7c52
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-10-24 11:18:14 +0300

    Update simpleCar.java

--- Java-Fundamentals/simpleCar.java
+++ Java-Fundamentals/simpleCar.java
@@ -1,25 +1,11 @@
-import java.util.Scanner;
-class SimpleCar {
-    private int speed;
+public class SimpleCar{
+    int speed;
 
-    public SimpleCar(int initialSpeed) {
-        this.speed = initialSpeed;
-    }
-
-    public void accelerate() {
-        speed += 10;
-        System.out.println("Speed after accelerate: " + speed);
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int iniitialSpeed = scanner.nextInt();
-        SimpleCar car = new SimpleCar(iniitialSpeed);
-
-        car.accelerate();
-        car.accelerate();
-        car.accelerate();
-
-    }
+        public SimpleCar(int initialSpeed) {
+            this.speed = initialSpeed;
+        }
+        public void accelerate(){
+            speed += 10;
+            System.out.println("Speed after accelerate: " + speed);
+        }
 }
