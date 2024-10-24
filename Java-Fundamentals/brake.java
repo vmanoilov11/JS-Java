302b6a86c4e5cb965ed3700034180921436373ba
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-10-24 11:21:48 +0300

    Create brake.java

---
+++ Java-Fundamentals/brake.java
@@ -0,0 +1,15 @@
+public class SimpleCar{
+    int speed;
+
+        public SimpleCar(int initialSpeed) {
+            this.speed = initialSpeed;
+        }
+        public void accelerate(){
+            speed += 10;
+            System.out.println("Speed after accelerate: " + speed);
+        }
+        public void brake() {
+        speed -= 20;
+        System.out.println("Speed after brake: " + speed);
+    }
+}
